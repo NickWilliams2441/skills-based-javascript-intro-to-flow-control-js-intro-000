3bcd1d386d5d798ea9ab4c7e98dd2622456e4f43
Nicholas Williams <nickdwilliams96@gmail.com>
2020-07-27 17:07:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,7 @@
 function basicTeenager(age) {
-
+if (age <=19 && >= 13){
+  return "You are a teenager!";
+}
 }
 
 function teenager(age) {
@@ -7,7 +9,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  
 }
 
 function ternaryTeenager(age) {
